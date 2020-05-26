bc50fa41d1240f21e794e9f0c4658c59d6d70f1e
tonydmorris <anthonymorris86@gmail.com>
2020-05-26 10:13:13 +0100

    added custom restaurant controller

--- api/restaurant/controllers/restaurant.js
+++ api/restaurant/controllers/restaurant.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  find: (ctx) => {
+    return strapi.query("restaurant").find(ctx.query, [
+      {
+        path: "menu_categories",
+        populate: {
+          path: "food_items",
+        },
+      },
+      {
+        path: "menu_deals",
+        populate: {
+          path: "menu_deal_options",
+        },
+      },
+    ]);
+  },
+};
